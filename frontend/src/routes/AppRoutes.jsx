2dbd021a774de48c03d6d18ec574439abb34e3cf
Chirag Soni <csoni0693@gmail.com>
2024-12-24 16:40:32 +0530

    ai

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import {BrowserRouter,Route,Routes} from 'react-router-dom'
 import Login from '../screens/Login'
 import Register from '../screens/Register'
+import Home from '../screens/Home'
 
 const AppRoutes = () => {
   return (
     <BrowserRouter>
     <Routes>
-        <Route path='/' element/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/login' element={<Login/>}/>
         <Route path='/register' element={<Register/>}/>
     </Routes>
