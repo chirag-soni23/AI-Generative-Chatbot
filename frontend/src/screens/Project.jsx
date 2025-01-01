a5a1dc263c72734b672b4c46cc650bb812db7f05
Chirag Soni <csoni0693@gmail.com>
2025-01-01 15:48:53 +0530

    ai

--- frontend/src/screens/Project.jsx
+++ frontend/src/screens/Project.jsx
@@ -286,43 +286,60 @@ const Project = () => {
                 )}
             </section>
             {/* right section */}
-            <section className="right bg-red-500 flex-grow h-full flex">
-                <div className="explorer bg-blue-500  h-full max-w-64 min-w-52">
-                    <div className="file-tree w-full">
+            <section className="right flex-grow h-full flex">
+    <div className="explorer bg-gray-300 h-full max-w-64 min-w-52">
+        <div className="file-tree w-full">
 
-                        {Object.keys(fileTree).map((file, index) => (
-                            <button onClick={() => {
-                                setCurrentFile(file),
-                                setOpenFiles([...openFiles,file])
-                            }
-                            } className="tree-element px-4 py-2 cursor-pointer flex items-center gap-2 bg-green-400 w-full ">
-                                <p className="cursor-pointer font-semibold tet-lg">{file}</p>
-                            </button>
-                        ))}
+            {Object.keys(fileTree).map((file, index) => (
+                <button
+                    onClick={() => {
+                        setCurrentFile(file);
+                        setOpenFiles([...new Set([...openFiles, file])]);
+                    }}
+                    className="tree-element px-4 py-2 cursor-pointer flex items-center gap-2 bg-slate-400 w-full hover:bg-slate-600"
+                >
+                    <p className="cursor-pointer font-semibold tet-lg text-white">
+                        {file}
+                    </p>
+                </button>
+            ))}
 
-                    </div>
-                </div>
-                {currentFile && (
-                <div className="code-editor flex  flex-col flex-grow h-full">
-                   <div className="top flex">
-                    {openFiles.map((file,index)=>{
-                        return <button className="open-file cursor-pointer p-2 px-4 items-center gap-2 bg-slate-50 w-fit" onClick={()=>setCurrentFile(file)}>
+        </div>
+    </div>
+    {currentFile && (
+        <div className="code-editor flex flex-col flex-grow h-full">
+            <div className="top flex">
+                {openFiles.map((file, index) => {
+                    return (
+                        <button
+                            className="open-file cursor-pointer p-2 px-4 items-center gap-2 bg-slate-100 hover:bg-slate-300 w-fit"
+                            onClick={() => setCurrentFile(file)}
+                        >
                             <p className="font-semibold text-lg">{file}</p>
                         </button>
-                    })}
-                   </div>
-                   <div className="bottom flex flex-grow">
-                    {fileTree[currentFile] &&(
-                        <textarea className="w-full h-full p-4 bg-slate-50 outline-none" value={fileTree[currentFile].content} onChange={(e)=>{setFileTree({...fileTree,[currentFile]:{
-                            content:e.target.value
-                        }})}}>
-                            
-                        </textarea>
-                    )}
-                   </div>
+                    );
+                })}
+            </div>
+            <div className="bottom flex flex-grow">
+                {fileTree[currentFile] && (
+                    <textarea
+                        className="w-full h-full p-4 bg-slate-50 outline-none"
+                        value={fileTree[currentFile].content}
+                        onChange={(e) => {
+                            setFileTree({
+                                ...fileTree,
+                                [currentFile]: {
+                                    content: e.target.value,
+                                },
+                            });
+                        }}
+                    ></textarea>
+                )}
+            </div>
+        </div>
+    )}
+</section>
 
-                </div>)}
-            </section>
         </main>
     );
 };
